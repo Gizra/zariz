95b9b152441ad50747ca214010ef9e63c002a662
Amitaibu <amitai@gizra.com>
2014-01-20 12:09:46 +0200

    Add Entity reference integration.
    * Add Selection handler so reference will show only correct values by context
    * Add Behavior selection to add UUID value along with the entity ID reference
    * When editing a node select exsiting items by UUID

---
+++ entityreference/behavior/ZarizEntityReferenceBehavior.class.php
@@ -0,0 +1,70 @@
+<?php
+
+/**
+ * @file
+ * Contains ZarizEntityReferenceBehavior
+ */
+
+class ZarizEntityReferenceBehavior extends EntityReference_BehaviorHandler_Abstract {
+
+  /**
+   * Overrides EntityReference_BehaviorHandler_Abstract::access().
+   */
+  public function access($field, $instance) {
+    return $field['settings']['handler'] == 'zariz' || strpos($field['settings']['handler'], 'zariz_') === 0;
+  }
+
+  /**
+   * Overrides EntityReference_BehaviorHandler_Abstract::schema_alter().
+   *
+   * Add UUID field.
+   */
+  public function schema_alter(&$schema, $field) {
+    $schema['columns']['uuid'] = array(
+      'description' => 'The UUID of the referenced entity.',
+      'type' => 'varchar',
+      'length' => 128,
+      'not null' => FALSE,
+      'default' => NULL,
+    );
+  }
+
+  /**
+   * Overrides EntityReference_BehaviorHandler_Abstract::presave().
+   *
+   * Add UUID along with the referenced entity ID.
+   */
+  public function presave($entity_type, $entity, $field, $instance, $langcode, &$items) {
+    $field_name = $field['field_name'];
+    $cardinality = $field['cardinality'];
+
+    $wrapper = entity_metadata_wrapper($entity_type, $entity);
+
+    $uuids = array();
+
+    if ($cardinality == 1) {
+      if ($id = $wrapper->{$field_name}->getIdentifier()) {
+        $uuids[$id] = $wrapper->{$field_name}->field_uuid->value();
+      }
+    }
+    else {
+      foreach ($wrapper->{$field_name} as $sub_wrapper) {
+        $id = $sub_wrapper->getIdentifier();
+        $uuids[$id] = $sub_wrapper->field_uuid->value();
+      }
+    }
+
+
+    foreach ($items as &$item) {
+      if (empty($item['target_id'])) {
+        $item['uuid'] = NULL;
+      }
+      else {
+        $id = $item['target_id'];
+        $item['uuid'] = $uuids[$id];
+      }
+    }
+  }
+
+
+}
---
+++ entityreference/selection/ZarizSelectionHandler.class.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @file
+ * Contains ZarizSelectionHandler.
+ */
+
+class ZarizSelectionHandler extends EntityReference_SelectionHandler_Generic {
+
+  /**
+   * Implements EntityReferenceHandler::getInstance().
+   */
+  public static function getInstance($field, $instance = NULL, $entity_type = NULL, $entity = NULL) {
+    return new self($field, $instance, $entity_type, $entity);
+  }
+
+  /**
+   * Overrides EntityReference_SelectionHandler_Generic::settingsForm().
+   */
+  public static function settingsForm($field, $instance) {
+    $form = EntityReference_SelectionHandler_Generic::settingsForm($field, $instance);
+    if (!empty($form['target_bundles'])) {
+      $target_type = $field['settings']['target_type'];
+      $form['target_bundles']['#options'] = zariz_get_group_content_bundles($target_type);
+    }
+
+    return $form;
+  }
+
+  /**
+   * Overrides EntityReference_SelectionHandler_Generic::buildEntityFieldQuery().
+   *
+   * Adds the "zariz" tag to the query.
+   */
+  public function buildEntityFieldQuery($match = NULL, $match_operator = 'CONTAINS') {
+    $handler = EntityReference_SelectionHandler_Generic::getInstance($this->field, $this->instance, $this->entity_type, $this->entity);
+    $query = $handler->buildEntityFieldQuery($match, $match_operator);
+
+    $query->addTag('zariz');
+    return $query;
+  }
+}
